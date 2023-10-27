57bb8ef76f9eb46e39df6e4cf51771a18f8b24cb
cj <115621312+bienjouejo@users.noreply.github.com>
2023-10-27 11:52:30 +0200

    Update initial_triage.py
    find.profile function fixed + output text files are now in a folder named result_initial_triage

--- initial_triage.py
+++ initial_triage.py
@@ -1,22 +1,27 @@
 import subprocess
 import os
 import sys
+import re
 
 def find_profile(dump_file):
     # Use volatility to find the profile for the given dump file
     command = f"volatility -f {dump_file} imageinfo"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     
-    # Extract the profile from the result
-    profile_lines = result.stdout.split('\n')
-    for line in profile_lines:
-        if line.startswith("Suggested Profile"):
-            return line.split(":")[1].strip()
+    # Extract the profile using regular expression
+    profile_match = re.search(r"Suggested Profile\(s\) : (.+)", result.stdout)
+    
+    if profile_match:
+        return profile_match.group(1).strip().split(",")[0]  # Get the first suggested profile
+    else:
+        return None
 
-    return None
-
-def run_volatility_command(dump_file, profile, command, output_file):
+def run_volatility_command(dump_file, profile, command, output_folder):
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+    
     # Execute volatility command with the specified profile
+    output_file = os.path.join(output_folder, f"{command}_output.txt")
     volatility_command = f"volatility -f {dump_file} --profile={profile} {command} > {output_file}"
     subprocess.run(volatility_command, shell=True)
 
@@ -27,6 +32,7 @@ def main():
         sys.exit(1)
 
     dump_file = sys.argv[1]
+    output_folder = "result_initial_triage"
     
     # Find the profile for the dump file
     profile = find_profile(dump_file)
@@ -37,11 +43,10 @@ def main():
         # Define Volatility commands to run
         volatility_commands = ["pslist", "netscan", "filescan"]
 
-        # Run each Volatility command and save output to a text file
+        # Run each Volatility command and save output to a text file in the output folder
         for command in volatility_commands:
-            output_file = f"{command}_output.txt"
-            run_volatility_command(dump_file, profile, command, output_file)
-            print(f"{command} command executed. Results saved to {output_file}")
+            run_volatility_command(dump_file, profile, command, output_folder)
+            print(f"{command} command executed. Results saved to {output_folder}/{command}_output.txt")
 
     else:
         print("Profile not found. Unable to proceed.")
