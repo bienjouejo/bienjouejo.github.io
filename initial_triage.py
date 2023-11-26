a55e2a55d7ea2bd8f0eb1cf7174f874ea1fc06d8
cj <115621312+bienjouejo@users.noreply.github.com>
2023-11-26 01:16:08 +0100

    Update initial_triage.py
    added subfolder browser with the result of consoles and iehistory

--- initial_triage.py
+++ initial_triage.py
@@ -17,9 +17,6 @@ def find_profile(dump_file):
         return None
 
 def run_volatility_command(dump_file, profile, command, output_folder):
-    # Create the output folder if it doesn't exist
-    os.makedirs(output_folder, exist_ok=True)
-    
     # Execute volatility command with the specified profile
     output_file = os.path.join(output_folder, f"{command}_output.txt")
     volatility_command = f"volatility -f {dump_file} --profile={profile} {command} > {output_file}"
@@ -33,18 +30,29 @@ def main():
 
     dump_file = sys.argv[1]
     output_folder = "result_initial_triage"
-    
+
     # Find the profile for the dump file
     profile = find_profile(dump_file)
 
     if profile is not None:
         print(f"Profile found: {profile}")
 
-        # Define Volatility commands to run
-        volatility_commands = ["pslist", "netscan", "filescan", "consoles", "iehistory", ""]
+        # Create a sub-folder named "browser"
+        browser_folder = os.path.join(output_folder, "browser")
+        os.makedirs(browser_folder, exist_ok=True)
 
-        # Run each Volatility command and save output to a text file in the output folder
+        # Define Volatility commands to run
+        volatility_commands = ["consoles", "iehistory"]  # Modified to include only these commands
+
+        # Run each Volatility command and save output to a text file in the appropriate folder
         for command in volatility_commands:
+            run_volatility_command(dump_file, profile, command, browser_folder)
+            print(f"{command} command executed. Results saved to {browser_folder}/{command}_output.txt")
+
+        # Additional commands to run
+        other_commands = ["pslist", "netscan", "filescan"]
+
+        for command in other_commands:
             run_volatility_command(dump_file, profile, command, output_folder)
             print(f"{command} command executed. Results saved to {output_folder}/{command}_output.txt")
 
